619e84493431e6a9fe1856bbc87f1282eafb0d83
mohamad hosein <mmdtrni@gmail.com>
2024-03-15 02:27:10 +0330

    [init: complete grid func]

--- script.js
+++ script.js
@@ -1,43 +1,3 @@
-// let gameWidth = 3;
-// let gameHeight = 3;
-
-// const game = document.getElementById("game");
-
-// const block = document.createElement("div");
-// function createBlock(i, j) {
-//   block.classList.add("block" + "_" + i + "_" + j);
-//   block.classList.add("block");
-//   game.appendChild(block.cloneNode(true));
-// }
-
-// let i = 1;
-// let j = 1;
-// for (i, j; i <= gameWidth || j <= gameHeight; i++, j++) {
-//   if (gameWidth === 0) {
-//     createBlock(i, j);
-//     gameHeight--;
-//   } else {
-//     createBlock(i, j);
-//     gameWidth--;
-//   }
-// }
-// for (j; j <= gameHeight; j++) {
-//   createBlock(i, j);
-//   gameHeight--;
-// }
-// let i = 1;
-// let j = 1;
-// while ((gameWidth !== 0) | (gameHeight !== 0)) {
-//   if (gameWidth === 0) {
-//     createBlock(i, j);
-//     i++;
-//     gameHeight--;
-//   }
-//   createBlock(i, j);
-//   j++;
-//   gameWidth--;
-// }
-
 // const blk = {
 //   func: function () {
 //     return "Hello World";
@@ -54,31 +14,6 @@
 // console.log(blk);
 // console.log(blk.func());
 
-let gameWidth = 8;
-let gameHeight = 8;
-let matrix = [];
-
-for (let i = 1; i <= gameHeight; i++) {
-  matrix[i] = [];
-  for (let j = 1; j <= gameWidth; j++) {
-    matrix[i][j] = i * gameWidth + j;
-  }
-}
-
-function createBlock(i, j) {
-  const block = document.createElement("div");
-  block.classList.add("block");
-  block.classList.add(`block_${i}_${j}`);
-  // block.innerText = value;
-  return block;
-}
-
-const game = document.getElementById("game");
-
-matrix.forEach((row, i) => {
-  row.forEach((value, j) => {
-    const blockElement = createBlock(i, j, value);
-    game.appendChild(blockElement);
-  });
-  game;
-});
+let gameWidth = 6;
+let gameHeight = 6;
+createGrid(gameWidth, gameHeight);
--- utilities.js
+++ utilities.js
@@ -18,6 +18,34 @@ function newGame() {
   // TODO: this function should restart the game
 }
 
+function createGrid(width, height) {
+  // this function should create grid of blocks by given width and height
+  let matrix = [];
+  for (let i = 1; i <= height; i++) {
+    matrix[i] = [];
+    for (let j = 1; j <= width; j++) {
+      matrix[i][j] = i * width + j;
+    }
+  }
+
+  function createBlock(i, j) {
+    const block = document.createElement("div");
+    block.classList.add("block");
+    block.classList.add(`block_${i}_${j}`);
+    return block;
+  }
+
+  const game = document.getElementById("game");
+
+  matrix.forEach((row, i) => {
+    row.forEach((value, j) => {
+      const blockElement = createBlock(i, j);
+      game.appendChild(blockElement);
+    });
+    game;
+  });
+}
+
 function isEmpty(block) {
   // TODO: this function return true if the block is empty
 }
