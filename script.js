1bbe160eae1d0e04afcb5db3f652bb45a5858ecb
mohamad hosein <mmdtrni@gmail.com>
2024-03-14 17:08:12 +0330

    [init: matrix and first try to grid]

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
-let gameWidth = 3;
-let gameHeight = 3;
+// let gameWidth = 3;
+// let gameHeight = 3;
 
-const game = document.getElementById("game");
+// const game = document.getElementById("game");
 
-console.log(game);
-
-const block = document.createElement("div");
-block.classList.add("block");
-game.appendChild(block);
-
-// while (gameWidth!==0) {
-// gameWidth--;
-// if (gameWidth === 0) {
-
-// }
+// const block = document.createElement("div");
+// function createBlock(i, j) {
+//   block.classList.add("block" + "_" + i + "_" + j);
+//   block.classList.add("block");
+//   game.appendChild(block.cloneNode(true));
 // }
 
-// for (let i; i <= gameWidth; i++) {
-//   for (let j; j <= gameHeight; j++) {
-//     game.appendChild(document.createElement("div"));
-//     console.log(game.lastChild);
+// let i = 1;
+// let j = 1;
+// for (i, j; i <= gameWidth || j <= gameHeight; i++, j++) {
+//   if (gameWidth === 0) {
+//     createBlock(i, j);
+//     gameHeight--;
+//   } else {
+//     createBlock(i, j);
+//     gameWidth--;
 //   }
 // }
+// for (j; j <= gameHeight; j++) {
+//   createBlock(i, j);
+//   gameHeight--;
+// }
+// let i = 1;
+// let j = 1;
+// while ((gameWidth !== 0) | (gameHeight !== 0)) {
+//   if (gameWidth === 0) {
+//     createBlock(i, j);
+//     i++;
+//     gameHeight--;
+//   }
+//   createBlock(i, j);
+//   j++;
+//   gameWidth--;
+// }
 
-// let blk = {
-//   status: "pressed",
+// const blk = {
+//   func: function () {
+//     return "Hello World";
+//   },
+//   status: "pressed", //pressed / unpressed
 //   dataX: 2,
 //   dataY: 3,
 //   isMine: true,
@@ -32,3 +50,35 @@ game.appendChild(block);
 //   flag: false,
 //   value: 3,
 // };
+
+// console.log(blk);
+// console.log(blk.func());
+
+let gameWidth = 6;
+let gameHeight = 6;
+let matrix = [];
+
+for (let i = 1; i <= gameHeight; i++) {
+  matrix[i] = [];
+  for (let j = 1; j <= gameWidth; j++) {
+    matrix[i][j] = i * gameWidth + j;
+  }
+}
+
+function createBlock(i, j) {
+  const block = document.createElement("div");
+  block.classList.add("block");
+  block.classList.add(`block_${i}_${j}`);
+  // block.innerText = value;
+  return block;
+}
+
+const game = document.getElementById("game");
+
+matrix.forEach((row, i) => {
+  row.forEach((value, j) => {
+    const blockElement = createBlock(i, j, value);
+    game.appendChild(blockElement);
+  });
+  game;
+});
